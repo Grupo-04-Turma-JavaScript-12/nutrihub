0970363aba92c95924149672481ddb8a4c2f695e
André César <andrecesarhenrique@gmail.com>
2026-01-29 09:04:25 -0300

    Alterando para ambiente de produção

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,15 +3,15 @@ import { ConfigModule } from '@nestjs/config';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { AuthModule } from './auth/auth.module';
 import { CategoriaModule } from './categoria/categoria.module';
-import { DevService } from './data/services/dev.service';
 import { RefeicaoModule } from './refeição/refeicao.module';
 import { RestauranteModule } from './restaurante/restaurante.module';
+import { ProdService } from './data/services/prod.service';
 
 @Module({
   imports: [
     ConfigModule.forRoot(),
     TypeOrmModule.forRootAsync({
-      useClass: DevService,
+      useClass: ProdService,
       imports: [ConfigModule],
     }),
     RefeicaoModule,
